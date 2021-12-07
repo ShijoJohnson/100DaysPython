b488aa37a261265b4409a390731706aa64502cb8
ShijoJ <shijovjohnson@gmail.com>
2021-12-07 01:11:55 -0500

    Initial commit

---
+++ Day23_Turtle_Cross_Game/car_manager.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.goto(280,-260)
+        self.turtlesize(stretch_wid=1, stretch_len=5)
