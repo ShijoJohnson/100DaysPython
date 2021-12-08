cd06792acf06426a611b518a9e316d6a0c2b3148
ShijoJ <shijovjohnson@gmail.com>
2021-12-08 00:04:15 -0500

    Initial commit

--- Day24_FileSystem/main.py
+++ Day24_FileSystem/main.py
@@ -1,1 +1,15 @@
-Hello, Shijo
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#     file.close()
+
+# with open("my_file.txt", mode = "w") as file:
+#     file.write("New text")
+
+
+# with open("my_file.txt", mode = "a") as file:
+#     file.write("\nAppended text")
+
+
+with open("new_file.txt", mode = "w") as file:
+    file.write("New text")
