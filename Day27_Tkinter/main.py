06e2c9a93ff6e7be2c2c9c3aaedf3b0a9c13f899
ShijoJ <shijovjohnson@gmail.com>
2021-12-21 23:40:30 -0500

    Initial commit

---
+++ Day27_Tkinter/main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("First GUI")
+window.minsize(width = 500, height = 300)
+
+my_label = tkinter.Label(text="my Label", font = ("Arial", 24, "bold"))
+my_label.pack()
+
+
+window.mainloop()
