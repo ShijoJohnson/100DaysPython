b763dd0cb084322bc37a95710a05f41051b5d088
ShijoJ <shijovjohnson@gmail.com>
2021-12-26 22:30:10 -0500

    Initial commit

--- Day27_Tkinter/main.py
+++ Day27_Tkinter/main.py
@@ -5,7 +5,34 @@ window.title("First GUI")
 window.minsize(width = 500, height = 300)
 
 my_label = tkinter.Label(text="my Label", font = ("Arial", 24, "bold"))
+#my_label.pack(side = "left")
 my_label.pack()
+
+#updating properties of an object
+my_label["text"] = "New Text"
+#or use this for above my_label.config(text = "new Text")
+
+#Entry
+input = tkinter.Entry(width =  10)
+input.pack()
+print(input.get())
+
+
+#button
+def button_clicked():
+    # my_label.config(text = "I was clicked")
+    my_label.config(text=input.get())
+    print("I was clicked!")
+
+button = tkinter.Button(text = "Click me", command = button_clicked)
+button.pack()
+
+
+
+
+
+
+
 
 
 window.mainloop()
--- Day27_Tkinter/playground.py
+++ Day27_Tkinter/playground.py
@@ -1,7 +1,31 @@
-def add(*args):
-    sum = 0
-    for i in args:
-        sum+=i
-    return sum
+# def add(*args):
+#     sum = 0
+#     for i in args:
+#         sum+=i
+#     return sum
+#
+# print(add(1,2,3,4,4))
 
-print(add(1,2,3,4,4))
+
+# def calculate(n, **kwargs):
+#     # print(kwargs)
+#     # for key, value in kwargs.items():
+#         # print(key)
+#         # print(value)
+#     n += kwargs["add"]
+#     n *= kwargs["mutiply"]
+#     print(n)
+#
+# calculate(2, add= 3, mutiply =2)
+
+class Car:
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        # self.model = kw["model"]
+        self.model = kw.get("modelllll") # this way initialization wont fail if no value is not sent
+
+
+my_car = Car(make ="Toyota", model = "Camry")
+print(my_car.make, my_car.model)
+
+
