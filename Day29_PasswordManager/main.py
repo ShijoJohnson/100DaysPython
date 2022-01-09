d5c27e0b4643f8f244284c8471a8ebac089e2829
ShijoJ <shijovjohnson@gmail.com>
2022-01-09 12:34:19 -0500

    Day 29 commit

--- Day29_PasswordManager/main.py
+++ Day29_PasswordManager/main.py
@@ -1,7 +1,53 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+def generate_password():
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for char in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for symbol in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for num in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    # password = ""
+    # for char in password_list:
+    #   password += char
+    password = "".join(password_list)
+
+    pwd_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = pwd_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title= "Oops", message="Please make sure you havent left any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"These are the details entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save?")
+        if is_ok:
+            with open("data.txt", 'a') as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                web_entry.delete(0, END)
+                pwd_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -22,17 +68,28 @@ password_label = Label(text="Password:")
 password_label.grid(column=0, row = 3)
 
 #Entries
-T = Text(window, height=2, width=35)
-T.grid(column=1, row = 1, columnspan = 2)
-T1 = Text(window, height=2, width=35)
-T1.grid(column=1, row = 2, columnspan = 2)
-T1 = Text(window, height=2, width=21)
-T1.grid(column=1, row = 3)
+# web_entry = Text(window, height=2, width=35)
+# web_entry.grid(column=1, row = 1, columnspan = 2)
+# web_entry.focus()
+# email_entry = Text(window, height=2, width=35)
+# email_entry.grid(column=1, row = 2, columnspan = 2)
+# email_entry.insert(END, "shijo.johnson@yahoo.ca")
+# pwd_entry = Text(window, height=2, width=21)
+# pwd_entry.grid(column=1, row = 3)
+web_entry = Entry(width=35)
+web_entry.grid(column=1, row = 1, columnspan = 2)
+web_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row = 2, columnspan = 2)
+email_entry.insert(0, "shijo.johnson@yahoo.ca")
+pwd_entry = Entry(width=21)
+pwd_entry.grid(column=1, row = 3)
+
 
 #Button
-generate_pwd_button = Button(text="Generate Password")
+generate_pwd_button = Button(text="Generate Password", command=generate_password)
 generate_pwd_button.grid(column=2, row = 3)
-Add_button = Button(text="Add", width = 36)
+Add_button = Button(text="Add", width = 36, command = save)
 Add_button.grid(column=1, row = 4, columnspan= 2)
 
 window.mainloop()
