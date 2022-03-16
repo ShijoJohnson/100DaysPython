7f59e117f75afadd0acf4b54f5587f1a2ad20227
ShijoJ <shijovjohnson@gmail.com>
2022-03-15 23:55:39 -0400

    First Commit Day 32

--- Day32_BirthdayWisher/Birthday Wisher (Day 32) start/main.py
+++ Day32_BirthdayWisher/Birthday Wisher (Day 32) start/main.py
@@ -3,8 +3,9 @@ import smtplib
 my_email = "testemail123@gmail.com"
 password = "Password123^&*"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user= my_email, password = password)
-connection.sendmail(from_addr=my_email, to_addrs="testemail123@gmail.com", msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user= my_email, password = password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="testemail123@gmail.com",
+                        msg="Subject:Hello\n\nThis is the body of email")
